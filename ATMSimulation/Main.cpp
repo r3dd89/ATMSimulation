994e3039b361a7f7f4b99add488a49b682a0dc90
Chanel Maughmer <chanel.maughmer@gmail.com>
2023-11-05 17:06:00 -0600

    Created the user menu. Upon testing I realized I didn't add the create account to the menu. So, you can't get passed the enter account prompt because of course there's no account to enter. So I'm working on implementing the user account menu

---
+++ ATMSimulation/Main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include "Account.h"
+#include "ATM.h"
+#include "Bank.h"
+
+
+int main()
+{
+	// Creates instances of the Bank and ATM classes
+	Bank myBank;
+	ATM myATM;
+
+	int accountNumber;
+	int pin;
+	int select;
+	double amount;
+
+	// Displays the welcome message
+	std::cout << "=============================\n" << std::endl;
+	std::cout << "Welcome to the Bank of Nerds\n" << std::endl;
+	std::cout << "=============================\n\n" << std::endl;
+
+	// Keeps track of when the user wants to finish using the ATM
+	bool userFinished = false;
+
+	// Main application loop
+	while (!userFinished)
+	{
+		// Asks the user to "Insert card" a.k.a account number
+		std::cout << "Please insert card (enter account number): ";
+		std::cin >> accountNumber;
+
+		// Ask user of input
+		std::cout << "Enter PIN: ";
+		std::cin >> pin;
+
+
+		// Attempts to find the account in the bank and verifies the PIN #
+		Account* userAccount = myBank.findAccount(accountNumber);
+		if (userAccount != nullptr && userAccount->validatePIN(pin))
+		{
+			// If the account is found and PIN is correct, proceed with ATM operations.
+			myATM.insertCard(*userAccount, pin);
+			bool sessionActive = true;
+			
+			// Session loop for ATM operations
+			while (sessionActive)
+			{
+				// Display menu options for user
+				std::cout << "1. Check Balance\n2. Deposit\n3. Withdraw\n 4. Exit\nSelect an option: ";
+				std::cin >> select;
+				// Handle user's menu selection
+				switch (select)
+				{
+				case 1:
+					// Show's the user's balance
+					myATM.showBalance();
+					break;
+
+				case 2:
+					// Handles deposit operation
+					std::cout << "Enter amount to deposit: ";
+					std::cin >> amount;
+					myATM.deposit(amount);
+					break;
+
+				case 3:
+					// Handles withdraw operation
+					std::cout << "Enter withdrawal amount: ";
+					std::cin >> amount;
+					myATM.withdraw(amount);
+					break;
+
+				case 4:
+					// Exit the session and eject the card
+					sessionActive = false;
+					myATM.ejectCard();
+					break;
+
+				default:
+					// Handles invalid entry
+					std::cout << "Invalid option." << std::endl;
+					break;
+				}
+			}
+		}
+		else
+		{
+			// Informs the user the account number or the PIN number is incorrect
+			std::cout << "Invalid account number or PIN number." << std::endl;
+		}
+
+		// Ask user if they want to perform another transaction
+		std::cout << " Would you like to perform another transaction? (y/n): ";
+		char continueTransaction;
+		std::cin >> continueTransaction;
+
+		// If user types n or N finish the loop
+		userFinished = (continueTransaction == 'n' || continueTransaction == 'N');
+	}
+	// Display good by message
+	std::cout << "Thank you for banking with Bank of Nerds. Goodbye!" << std::endl;
+
+	return 0;
+}
