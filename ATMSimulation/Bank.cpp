fa124b31b9a28a4495f9a1230debda49b9d45842
Chanel Maughmer <chanel.maughmer@gmail.com>
2023-11-05 20:01:43 -0600

    Added code that generates a random number when

--- ATMSimulation/Bank.cpp
+++ ATMSimulation/Bank.cpp
@@ -1,6 +1,7 @@
 #include "Bank.h"
+#include <random>
 
-int Bank::_newAccountNumber = 9999;
+int Bank::_newAccountNumber;
 
 Bank::Bank()
 {
@@ -14,8 +15,30 @@ Account* Bank::createAccount(const std::string& name, int pin, double initialDep
 	{
 		throw std::invalid_argument("Initial deposit must be at least $25");
 	}
+	
 
-	_accounts.emplace_back(_newAccountNumber, name, pin, initialDeposit);
+	// Generates random account number
+	std::random_device rd; // Used to obtain a seed for the random number
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(1000, 9999); // Generates a number in this range
+	int accountNumber = dis(gen);
+
+	// Check if the account number already exist
+	auto checkNumberExist = std::find_if(_accounts.begin(), _accounts.end(), [accountNumber] (const Account & acc)
+	{
+		return acc.getAccountNumber() == accountNumber;
+	});
+
+	// If the account number already exists, generate a new number
+	while (checkNumberExist != _accounts.end())
+	{
+		accountNumber = dis(gen);
+		auto checkNumberExist = std::find_if(_accounts.begin(), _accounts.end(), [accountNumber](const Account& acc)
+			{
+				return acc.getAccountNumber() == accountNumber;
+			});
+	}
+_accounts.emplace_back(accountNumber, name, pin, initialDeposit);
 
 	// Returns a pointer to the new account
 	return &_accounts.back();
